7d3627da0ad9d6568ecddc6817ac2732fc3beff9
ixnp <enprocopios@gmail.com>
2022-09-15 12:09:48 -0700

    working on solutoin

---
+++ 03_oop/debug.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+from lib.pet import Pet
+
+
+import ipdb; ipdb.set_trace()
---
+++ 03_oop/lib/pet.py
@@ -0,0 +1,16 @@
+# Demonstrate classes 
+# 1. ✅ Create a Pet class
+# 2. ✅ Instantiate a pet instance 
+
+class Pet:
+    pass
+# Demonstrate instances 
+    # Different Instances are Different Objects
+# Demonstrate __init__
+# Demonstrate instance method
+# Demonstrate the self keyword 
+# Stretch Goals
+# Demonstrate object properties
+
+# Instances 
+rose = Pet()
