5e8f32fada3c0414e1037cf76d8542c6b0a4a8ab
Rachel Katz <rachel.katz@flatironschool.com>
2023-12-13 17:56:20 -0500

    removed solution

--- lib/owner.py
+++ lib/owner.py
@@ -1,45 +1,19 @@
 # Stretch Goal: Build Out Corresponding Owner Class Methods
 
-from pet import Pet 
-from appointments import Appointment
 class Owner:
     all = []
     def __init__(self, name, phone, email):
         self.name = name 
         self.phone = phone 
         self.email = email 
-        Owner.all.append(self)
 
     #✅ 3. create relationship: owner has many pets
-    def pets(self):
-        return [pet for pet in Pet.all if pet.owner == self]
-    
+
     def __repr__(self):
         return f'''<Owner {self.name} >'''
     
     #✅ 9. create a function for the total bill of the owner
     #✅ 9a. create helper method for all appointments of owner (owner has many appointments through pets)
-    def owner_appointments(self):
-        owner_apps = []
-        for pet in self.pets():
-            for app in Appointment.all:
-                if pet == app.pet:
-                    owner_apps.append(app)
-        return owner_apps
     
-    def total_bill(self):
-        bill = 0
-        for app in self.owner_appointments(): 
-            bill += app.procedure.price 
-        return bill
-
-    def appointments(self):
-        apps = []
-        for i in self.pets():
-            for app in Appointment.all:
-                if(app.pet == i):
-                    apps.append(i.name)
-        return apps 
-
 
     
--- lib/pet.py
+++ lib/pet.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
 # Class Attributes and Methods 
 import ipdb
-from appointments import Appointment
 class Pet:
 
-    all = []
     #✅ 1. create relationship: pet belongs to an owner
     #✅ 2. use chatGPT to create owners and pets in debug.py
     #✅ 2a. create __repr__ function to more easily read output
@@ -13,17 +11,10 @@ class Pet:
         self.age = age
         self.breed = breed
         self.owner = owner 
-        Pet.all.append(self)
 
     #✅ 5. create relationship: pet has many appointments
-    def appointments(self):
-        # return [appointment.request for appointment in Appointment.all if appointment.pet == self ]
-        return [appointment for appointment in Appointment.all if appointment.pet == self]
-    
+
     #✅ 6. create relationship: pet has many procedures (but make it unique)
-    #🛑 can't do vice versa in Procedure because we imported Procedure in appointments and we would get a circular import
-    def procedures(self):
-        return list(set([app.procedure.name for app in self.appointments()]))
 
     def print_pet_details(self):
         return(f'''
