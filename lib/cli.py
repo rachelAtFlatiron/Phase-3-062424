3230935120ea163fd0c0a9d78b149ecc3b053abb
Rachel Katz <rachel.katz@flatironschool.com>
2024-08-16 15:07:35 -0400

    demo done

--- lib/cli.py
+++ lib/cli.py
@@ -1,23 +1,68 @@
-# ✅ 9. Import all helper functions and necessary models
+# ORM (models) -> helpers.py (useful for creating relationships, managing the belongs to/has many) -> cli.py (main menu, infinite loop, if statements, what to do when a user inputs a specific number)
 
+
+# ✅ 9. Import all helper functions and necessary models
+from helpers import (
+    print_all_owners,
+    print_all_pets_for_owner
+)
 from models.owner import Owner 
 from models.pet import Pet
 
-# ✅ 14. Create a global `owner` and `pet` value
-cur_owner = None
+global cur_owner 
+cur_owner = None 
 cur_pet = None 
 
-# ✅ 10. Create a function `main()` that will wait for a user's input 
+#the main loop that constantly asks what the user wants to do next (including exiting the program)
+# exit()
 def main():
-    pass
-# ✅ 15. Add the decision tree to `main`
+    menu()
+    while(True):
+        choice = input("Enter something")
+        if(int(choice) == 0):
+            exit()
+        elif(int(choice) == 1):
+            #each if statement will probably execute a helper function
+            print_all_owners() 
+        elif(int(choice) == 2):
+            print_all_owners()
+            set_cur_owner()
+            print(f'Owner is currently: {cur_owner}')
+        elif(int(choice) == 3):
+            # get all pets where id equals owner
+            if(not cur_owner):
+                print("please select owner first")
+            else:
+                print_all_pets_for_owner(cur_owner.id)
+            # print out all those pets 
+        #if the user types something invalid
+        elif(int(choice) == 4):
+            #add new pet
+            pass 
+        elif(int(choice) == 5):
+            #delete pet
+            pass 
+        elif(int(choice) == 6):
+            #make appointment
+            pass 
+        else:
+            menu()
 
-# ✅ 13. Create a `menu` function that displays actions a user can take
+
+def set_cur_owner():
+    global cur_owner 
+    choice = input("Enter number of owner: ")
+    cur_owner = Owner.find_by_id(int(choice))
+
 def menu():
-    pass
-# ✅ 11. Create a function `greeting()` and `get_name()` that welcomes the user to the program and asks for their name 
+    print (f"""  
+        Current owner is {cur_owner}
+        Enter 0 to exit \n 
+        Enter 1 to see all owners \n
+        Enter 2 to select new owner \n
+        Enter 3 to see selected owners pets \n
+    """)
 
-# ✅ 12. Use `greeting` and `main` to start the program
 
 if __name__ == "__main__":
-    pass
+    main()
--- lib/helpers.py
+++ lib/helpers.py
@@ -16,11 +16,16 @@ def print_pet(id):
 
 # ✅ 4. A function `print_all_owners()` that prints all owners 
 def print_all_owners():
-    pass
+    # get all the owners 
+    owners = Owner.get_all()
+    for owner in owners:
+        print(f'{owner.id}. {owner.name}')
 
 # ✅ 5. A function `print_all_pets_for_owner(id)` that prints all pets for the owner with the id `id`
 def print_all_pets_for_owner(id):
-    pass
+    pets = Pet.get_owner_pets(id)
+    for pet in pets:
+        print(f'{pet.id}. {pet.name}')
 
 # ✅ 6. A function `print_all_apps_for_owner(id)` that prints all appointments for all the pets for owner with id `id`
 def print_all_apps_for_owner(id):
@@ -33,3 +38,5 @@ def print_apps_for_one_pet(id):
 # ✅ 8. A function `exit_program()` that prints will exit our cli
 def exit_program():
     pass
+
+# import ipdb; ipdb.set_trace()
